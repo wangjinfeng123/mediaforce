9a813ba40c731a20918e4a9c48b30f7187f3ec6f
wangjinfeng123 <jinfeng.wang@baifendian.com>
2016-11-29 14:40:41 +0800

    var.py222444

---
+++ var.py
@@ -0,0 +1,8 @@
+# _*_ coding: cp936 _*_
+import random
+_all_ = ['testurl','searchtext','list_vars','integer']
+
+testurl='https://www.baidu.com/'
+searchtext=u'文件形式搜索'
+list_vars=['hello','list','word']
+integer=random.randint(1,10)
